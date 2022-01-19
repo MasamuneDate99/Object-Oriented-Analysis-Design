308e7080169b1f985b919b394c2d86fd4a4f7d1a
Zynn02 <robertio2202001@gmail.com>
2022-01-19 22:28:57 +0700

    test
    test

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,9 +5,12 @@ public class Main {
 
 	public Main() {
 		// TODO Auto-generated constructor stub
-		
+		// test
 	}
 
+	public void main2() {
+		
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		new Main();
