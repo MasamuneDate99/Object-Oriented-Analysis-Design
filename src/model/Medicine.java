32e64e0f6f49b60387a09ca46c8cec111cc62989
TYR <natdavin@gmail.com>
2022-01-20 11:03:23 +0700

    Revert Change
    Revert Change

--- src/model/Medicine.java
+++ src/model/Medicine.java
@@ -1,8 +1,18 @@
 package model;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Vector;
+
+import connect.Connect;
+
 public class Medicine {
 	private int MedicineID, MPrice, MStock;
 	private String MName;
+	private String table = "medicine";
+	
+	private Connect con = Connect.getConnection();
 	//private Connect con = Connect.getConnection();
 	
 	public Medicine(int medicineID, int mPrice, int mStock, String mName) {
@@ -35,5 +45,50 @@ public class Medicine {
 	}
 	public void setMName(String mName) {
 		MName = mName;
+	}
+	
+	public Vector<Medicine> GetAllMedicine(){
+		String query = String.format("SELECT * FROM %s", this.table);
+		
+		//menampung hasil dari select
+		ResultSet rs = con.executeQuery(query);
+		
+		//return data dari db 
+		Vector<Medicine> listMedicine = new Vector<>();
+		
+		//looping masukkin data ke vector
+		try {
+			while(rs.next()) {
+				int medicineID = rs.getInt("MedicineID");
+				String mName = rs.getString("MName");
+				int mPrice = rs.getInt("MPrice");
+				int mStock = rs.getInt("MStock");
+				
+				listMedicine.add(new Medicine(medicineID, mPrice, mStock, mName));			
+			}
+		return listMedicine;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	//insert medicine	
+	public boolean insertMedicine() {
+		String query = String.format("INSERT INTO %s Values (NULL,?,?,?)", this.table);
+		
+		PreparedStatement ps = con.prepareStatement(query);
+		
+		try {
+			ps.setString(1, MName);
+			ps.setInt(2, MPrice);
+			ps.setInt(3, MStock);
+			return ps.executeUpdate() == 1;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
 	}
 }
